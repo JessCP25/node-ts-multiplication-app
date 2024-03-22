d2aea20f97010c0e90fa0ee6f63d243df66a0320
JessCP25 <jpayanoc25@gmail.com>
2024-03-21 20:22:30 -0500

    Agregando use-case save-file

---
+++ src/domain/use-cases/save-file.use-case.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+export interface SaveFileUseCase {
+  execute: (options: Options) => boolean;
+}
+
+export interface Options {
+  fileContent: string;
+  fileDestination?: string;
+  fileName?: string;
+}
+
+export class SaveFile implements SaveFileUseCase {
+  constructor() {}
+
+  execute({
+    fileContent,
+    fileDestination = "outputs",
+    fileName = "table",
+  }: Options): boolean {
+    try {
+      fs.mkdirSync(fileDestination, { recursive: true });
+      fs.writeFileSync(`${fileDestination}/${fileName}.txt`, fileContent);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,17 +1,26 @@
 import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
 
 interface RunOptions {
-  base: number,
-  limit: number,
-  showTable: boolean
+  base: number;
+  limit: number;
+  showTable: boolean;
 }
 
 export class ServerApp {
-  static run({base, limit, showTable}: RunOptions){
-    console.log('Server running...');
-    const table = new CreateTable()
-    .execute({base, limit});
+  static run({ base, limit, showTable }: RunOptions) {
+    console.log("Server running...");
+    const table = new CreateTable().execute({ base, limit });
 
-    if(showTable) console.log(table);
+    const wasCreated = new SaveFile().execute({
+      fileContent: table,
+      fileDestination: `outputs/table-${base}`,
+    });
+
+    if (showTable) console.log(table);
+
+    wasCreated
+      ? console.log("File created!")
+      : console.log("File not created!");
   }
 }
